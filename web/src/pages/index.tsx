3cb76840243d69e94537dd0c3706f76aa8f4d3b1
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2022-11-14 13:15:00 -0300

    Organizando arquivos.

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Diel Task Manager</h1>
+        </div>
+        
+        <div></div>
+      </div>
+    </>
+  )
+}
