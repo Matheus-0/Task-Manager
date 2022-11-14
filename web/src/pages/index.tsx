39deb7c17c0cce243637995a64c91fc4c53f8fb7
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2022-11-14 16:50:00 -0300

    Alterando botão de buscar.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -77,18 +77,17 @@ export default function Home(props: HomeProps) {
                   id="simple-search"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
                   placeholder="Buscar tarefas"
-                  required
                   onChange={(e) => setSearch(e.target.value)}
                   value={search}
                 />
             </div>
 
-            <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-500 rounded-lg border border-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <button type="submit" className="flex p-2.5 ml-2 text-sm font-medium text-white bg-blue-500 rounded-lg border border-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                 </svg>
                 
-                <span className="sr-only">Buscar</span>
+                Buscar
             </button>
         </form>
 
